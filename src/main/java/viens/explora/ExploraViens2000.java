3f4ea97447a8b0672a7023ad7bea2b2a9336dea5
Louis-Philippe Viens <2034421@cegepmontpetit.ca>
2023-09-13 14:11:06 -0400

    FCT Exploration TODO 1
    Exploration de la première page

--- src/main/java/viens/explora/ExploraViens2000.java
+++ src/main/java/viens/explora/ExploraViens2000.java
@@ -1,37 +1,73 @@
 package viens.explora;
 
+import com.sun.org.apache.xpath.internal.Arg;
+import com.sun.org.apache.xpath.internal.objects.XString;
 import javafx.scene.control.Tab;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ExploraViens2000 {
 
     public static void main(String[] args) {
-        //On test s'il y a 2 arguments
+        //ÉTAPE 1 TODO 1: Arguments
+
+        // Argument 1/4 On test s'il y a 2 arguments
         if (args.length != 2){
             System.out.println("Merci de nous fournir 2 arguments :\n" +
                     "l'URL de la page de départ, la liste de mots clés à ignorer séparés par des espaces entre guillemets.\n" +
                     "Par exemple : https://info.cegepmontpetit.ca/3N5-Prog3/z/index.html \"pipo popi\"\n" +
                     "\n");
         }
-        //on test si l'URL est bien formée
+        //Argument 2/4 on test si l'URL est bien formée
         if(!isURLTextValid(args[0]))
             System.out.println("Nous avons rencontré un problème avec l'URL fournie : " + args[0] + "\nMerci de fournir une URL bien formée");
 
-        //on confirme si l'URL est valid
+        //Argument 3/4 on confirme si l'URL est valid
         if(!isURlValid(args[0]))
             System.out.println("Il n'y a pas de page correspondante à l'URL : " + args[0] + "\nMerci de fournir une URL correspondant à une page");
 
-        //On valide que la liste de mots clés ne contient pas de répétitions.
+        //Argument 4/4 On valide que la liste de mots clés ne contient pas de répétitions.
         if (!isListValid(args[1]))
         {
             System.out.println("Mots clés fournis incorrects " + args[1]);
             System.out.println("Merci de ne pas avoir de répétitions dans les mots clés : ");
         }
-        
+
+        //ÉTAPE 2 TODO "Exploration"
+        System.out.println("Les arguments sont corrects, nous commençons l'exploration de " + args[0]);
+        //Exploration 1 : afficher le titre et l'url complet
+        try{
+            String url = args[0];
+            Document doc = Jsoup.connect(url).get();
+            String titre = doc.title();
+            System.out.println("Titre : " + titre + "          URL : " + args[0]);
+            Elements links = doc.select("a");
+            if (!links.isEmpty()) {
+                // Extraire le premier lien trouvé
+                Element firstLink = links.first();
+
+                // Récupérer l'URL du premier lien
+                String firstLinkUrl = firstLink.attr("href");
+
+                // Afficher l'URL du premier lien
+                System.out.println("Premier lien de la page : " + firstLinkUrl);
+            } else {
+                System.out.println("Aucun lien trouvé sur la page.");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Erreur");
+        }
+
+
     }
+    //Début Region TODO 1
     public static boolean isURLTextValid(String url){
         //on test si l'URL est bien formée
         try{
@@ -69,8 +105,7 @@ public class ExploraViens2000 {
             return false;
         }
     }
-    public  static boolean isTabValid(String[] tab)
-    {
+    public  static boolean isTabValid(String[] tab){
         for (int a = 0; a <tab.length; a++)
         {
             for (int b= 0; b <tab.length; b++)
@@ -83,5 +118,11 @@ public class ExploraViens2000 {
         }
         return true;
     }
+    //Fin Region TODO 1
+    
+    //Début Region TODO 2
 
+    
+    
+    //Fin Region TODO 2
 }
